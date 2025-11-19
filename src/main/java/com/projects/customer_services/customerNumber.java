e28c3101fd6fe4960f6b7e0294f93d8911ee7c1f
Prasanth L <prasanthlingan@gmail.com>
2025-11-19 14:45:26 +0530

    Endpoint-1 Controller class

---
+++ src/main/java/com/projects/customer_services/customerNumber.java
@@ -0,0 +1,19 @@
+package com.projects.customer_services;
+
+public class customerNumber {
+        public static class accountNumber {
+        private long accountNumber;
+
+        //Getter & Setter
+
+
+        public long getAccountNumber() {
+            return accountNumber;
+        }
+
+        public void setAccountNumber(long accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+    }
+
+}
---
+++ src/main/java/com/projects/customer_services/userDTO.java
@@ -0,0 +1,69 @@
+package com.projects.customer_services;
+
+public class userDTO {
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String location;
+    private Long income;
+    private Long creditScore;
+    private int houseId;
+
+    //getter and Setter
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Long getIncome() {
+        return income;
+    }
+
+    public void setIncome(Long income) {
+        this.income = income;
+    }
+
+    public Long getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(Long creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public int getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(int houseId) {
+        this.houseId = houseId;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
