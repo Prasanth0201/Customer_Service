28b04cdd18fdfe49b61df26f3c6f821d1ec76c2e
Umesh <umeshsjkumar@gmail.com>
2025-12-05 12:56:56 +0530

    improvement

--- src/main/java/com/projects/customer_services/controller/customerController.java
+++ src/main/java/com/projects/customer_services/controller/customerController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("/api/")
 public class customerController {
     CustomerService service = new CustomerService();
-    @PostMapping("/fetchAccountDetails/")
+    @PostMapping(value = "/fetchAccountDetails/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDetails fetchUserDetails (@RequestBody AccountNumberRequest request){
         return service.fetchUserDetails(request.getAccountNumber());
     }
